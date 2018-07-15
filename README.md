9d3162af7b96f396ff13cf766c36ca6e495c01a6
edoner <edonet@163.com>
2018-07-15 11:05:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # cli
 Arted shell utils
+
+## Installation
+npm
+``` shell
+$ npm install -g @arted/cli
+```
+
+or yarn
+``` shell
+$ yarn global add @arted/cli
+```
+
+## Usage
+``` shell
+
+# zip dir to dir.zip
+$ dzip dir dir.zip
+
+# unzip dir.zip
+$ dzip -x dir.zip
+```
---
+++ bin/dzip.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+
+/**
+ *****************************************
+ * Created by lifx
+ * Created on 2018-07-13 10:09:02
+ *****************************************
+ */
+'use strict';
+
+
+/**
+ *****************************************
+ * 加载依赖
+ *****************************************
+ */
+const
+    yargs = require('yargs'),
+    { zip, unzip } = require('@arted/utils/zip'),
+    { error } = require('@arted/utils/stdout');
+
+
+/**
+ *****************************************
+ * 定义任务
+ *****************************************
+ */
+async function run() {
+    let argv = yargs.boolean('x').argv;
+
+    console.log(argv);
+
+    if (argv.x) {
+        let src = argv.src || argv._[0] || 'src.zip',
+            dist = argv.dist || argv._[1] || src.replace(/\.zip$/, '');
+
+
+        // 执行解缩
+        await unzip(src, dist);
+    } else {
+        let src = argv.src || argv._[0] || 'src',
+            dist = argv.dist || argv._[1] || src + '.zip';
+
+        // 执行压缩
+        await zip(src, dist);
+    }
+}
+
+
+/**
+ *****************************************
+ * 启动任务
+ *****************************************
+ */
+module.exports = run().catch(error);
